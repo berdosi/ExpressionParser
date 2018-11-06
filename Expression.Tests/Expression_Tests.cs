737b82c03451210ec4019d39142e1a297d237085
Balint Erdosi <erdosib@gmail.com>
2018-11-07 00:16:57 +0100

    initial commit

---
+++ Expression.Tests/Expression_Tests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Automation;
+
+namespace Automation.UnitTests {
+	public class Expression_Tests {
+		private Expression _expression;
+
+		public Expression_Tests() {
+		}
+
+		[Fact]
+		public void canBeCreated() {	
+			//_expression = new Automation.Expression("[variable]");
+			try
+			{
+				_expression = new Expression("[variable]");
+				Assert.True(true, "Object is initialized.");		
+			}
+			catch (System.Exception)
+			{
+				Assert.True(false, "Cannot initialize object.");
+			}
+		}
+	}
+}
---
+++ Expression/Expression.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Automation
+{
+    public class Expression
+    {
+        private List<Atom> Atoms;
+        public Expression(string inputString)
+        {
+            string input = inputString;
+            bool inString = false;
+            Regex inStringDoubleQuote = new Regex("^\"\"");
+            Regex inStringNoQuote = new Regex("^[^\"]+");
+            Regex finalQuote = new Regex("^\"");
+            int level = 0;
+            return;
+            while (input != "") {
+                if (!inString) {
+                } else {
+                    if (inStringDoubleQuote.IsMatch(input)) {
+                        var atom = Atoms[Atoms.Count -1];
+                        atom.contentString += "\"";
+                        input = input.Substring(2);
+                    } else if (inStringNoQuote.IsMatch(input)) {
+                        var atom = Atoms[Atoms.Count -1];
+                        atom.contentString += inStringNoQuote.Match(input).Value;
+                        input = input.Substring(inStringNoQuote.Match(input).Value.Length);
+                    } else if (finalQuote.IsMatch(input)) {
+                        inString = false;
+                        input = input.Substring(1);
+                    }
+                }
+            }
+        }
+    }
+    enum AtomType
+    {
+        FunctionCall,
+        Number,
+        Operator,
+        Paren_Close,
+        Paren_Open,
+        String,
+        Variable,
+    }
+    class Atom
+    {
+        public string contentString;
+        public Atom(AtomType type, string content) {
+
+        }
+    }
+}
