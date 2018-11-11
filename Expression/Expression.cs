4b38a965eb240080185f39820a77bb792bb3cf9a
Balint Erdosi <erdosib@gmail.com>
2018-11-12 00:01:40 +0100

    EncapsulatedData.Equals, GetHashCode

--- Expression/Expression.cs
+++ Expression/Expression.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
@@ -348,6 +349,67 @@ namespace Automation
         {
             type = DataType.ParameterList;
             parameterList = data;
+        }
+
+        public override bool Equals(object obj)
+        {            
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            
+            if (this.type != ((EncapsulatedData)obj).type) return false;
+            switch (this.type)
+            {
+                case DataType.Number:
+                    return this.numberData == ((EncapsulatedData)obj).numberData;
+                case DataType.String:
+                    return this.stringData == ((EncapsulatedData)obj).stringData;
+                case DataType.Boolean:
+                    return this.booleanData == ((EncapsulatedData)obj).booleanData;
+                case DataType.DateTime:
+                    return this.dateTimeData == ((EncapsulatedData)obj).dateTimeData;
+                case DataType.Date:
+                    throw new NotImplementedException("Separate Date type not implemented.");
+                case DataType.ParameterList:
+                    return System.Linq.Enumerable.SequenceEqual(
+                        this.parameterList,
+                        ((EncapsulatedData)obj).parameterList);
+                default:
+                    throw new NotImplementedException("Datatype not implemented for comparison.");
+            }
+        }
+        
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            // TODO: write your implementation of GetHashCode() here
+            int typeCode = this.type.GetHashCode();
+            int valueCode;
+            switch (this.type)
+            {
+                case DataType.Number:
+                    valueCode = numberData.GetHashCode();
+                    break;
+                case DataType.String:
+                    valueCode = stringData.GetHashCode();
+                    break;
+                case DataType.Boolean:
+                    valueCode = booleanData.GetHashCode();
+                    break;
+                case DataType.DateTime:
+                    valueCode = dateTimeData.GetHashCode();
+                    break;
+                case DataType.Date:
+                    throw new NotImplementedException("Date type not implemented.");
+                case DataType.ParameterList:
+                    valueCode = parameterList.GetHashCode();
+                    break;
+                default:
+                    valueCode = 0;
+                    break;
+            }
+            return typeCode ^ valueCode;
         }
     }
 }
